06b48b91761defa225948abd35bf806064a0b4b8
Lauris Zaicevs <lauris.zaicevs@abcsoftware.lv>
2021-06-03 13:39:28 +0300

    add countdown flip function

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
 import React, { Fragment } from "react";
 import Countdown from "react-countdown";
 
+import CountdownContainer from "./components/CountdownContainer";
+
 function App() {
   return (
     <Fragment>
       <div className="wrapper">
         <div className="main">
           <div className="main__title">We`re lauching soon</div>
-          <div className="main__countdown-container">
-            <Countdown
-              date={Date.now() + 120000000}
-              autoStar
-              renderer={({ days, hours, seconds, minutes }) => {
-                return (
-                  <Fragment>
-                    <div className="unit-box">
-                      <div className="unit-box__number">
-                        <div className="unit-box__number__layer-top"></div>
-                        <div className="unit-box__number__layer"> </div>
-                        <span>{days}</span>
-                      </div>
-                      <div className="unit-box__text">days</div>
-                    </div>
-                    <div className="unit-box">
-                      <div className="unit-box__number">
-                        <div className="unit-box__number__layer-top"></div>
-                        <div className="unit-box__number__layer"> </div>
-                        <span>{hours}</span>
-                      </div>
-                      <div className="unit-box__text">hours</div>
-                    </div>
-                    <div className="unit-box">
-                      <div className="unit-box__number">
-                        <div className="unit-box__number__layer-top"></div>
-                        <div className="unit-box__number__layer"> </div>
-                        <span>{minutes}</span>
-                      </div>
-                      <div className="unit-box__text">minutes</div>
-                    </div>
-                    <div className="unit-box">
-                      <div className="unit-box__number">
-                        <div className="unit-box__number__layer-top"></div>
-                        <div className="unit-box__number__layer"> </div>
-                        <span>{seconds}</span>
-                      </div>
-                      <div className="unit-box__text">seconds</div>
-                    </div>
-                  </Fragment>
-                );
-              }}
-            />
-          </div>
+
+          <Countdown
+            date={Date.now() + 120000000}
+            autoStar
+            renderer={({ formatted: { days, hours, seconds, minutes } }) => {
+              return (
+                <div className="main__countdown-container">
+                  <CountdownContainer
+                    days={days}
+                    hours={hours}
+                    minutes={minutes}
+                    seconds={seconds}
+                  />
+                </div>
+              );
+            }}
+          />
         </div>
         <footer>
           <div className="social-box">
